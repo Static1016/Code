1f8da61671dd54422dd721e59e2162a76449d6cb
Yash Mhaparle <yashmhaparle2004@gmail.com>
2025-12-14 10:59:44 +0530

    feat: Add CRUD API's

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,12 @@ app.use(express.json());
 
 // Routes import
 import userRouter from './routes/user.route.js'
+import postRouter from './routes/post.route.js'
 
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/post", postRouter);
 
 //example route: http
 
